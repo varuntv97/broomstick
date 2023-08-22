a8cb071ddb0d18839a77848c5bf8313dfeb397c9
varuntv <varun_tv@outlook.com>
2023-08-22 15:46:06 +0530

    untoldstory

---
+++ src/pages/HomePage/HomeUntoldStory.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeUntoldStory() {
+  return (
+    <div className='untoldStory_container'>HomeUntoldStory</div>
+  )
+}
+
+export default HomeUntoldStory
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,12 +3,14 @@ import Layout from "../layout/Layout"
 import HomeLanding from "./HomePage/HomeLanding"
 import '../styles/pages/HomePage/HomePage.scss'
 import HomeProductSpotlight from "./HomePage/HomeProductSpotlight"
+import HomeUntoldStory from "./HomePage/HomeUntoldStory"
 
 const IndexPage = () => {
   return (
     <Layout>
       <HomeLanding/>
       <HomeProductSpotlight/>
+      <HomeUntoldStory/>
     </Layout>
   )
 }
