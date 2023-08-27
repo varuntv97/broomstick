59f7a28d8c1064f5162af2c68694a6ff20196d99
varuntv <varun_tv@outlook.com>
2023-08-27 14:11:22 +0530

    backedby update

--- src/pages/HomePage/HomeBackedBy.jsx
+++ src/pages/HomePage/HomeBackedBy.jsx
@@ -1,10 +1,30 @@
-
-import React from 'react'
+import React from "react";
+import { StaticImage } from "gatsby-plugin-image";
 
 function HomeBackedBy() {
   return (
-    <div>HomeBackedBy</div>
-  )
+    <div className="backedByContainer">
+      <div className="backedByWrapper">
+        <div className="backedBy_title">
+          <h1>We are backed by</h1>
+        </div>
+        <div className="backedBy_grid1">
+            <StaticImage
+              src="../../images/HomePage/TIE2.png"
+              alt="TIE"
+            />
+            <StaticImage
+              src="../../images/HomePage/Ktech.png"
+              alt="TIE"
+            />
+            <StaticImage
+              src="../../images/HomePage/start-up karnataka.png"
+              alt="TIE"
+            />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default HomeBackedBy
+export default HomeBackedBy;
