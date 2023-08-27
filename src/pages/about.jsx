2cf719e2ce6b2decd1e42c515ea66341cb84da18
ruchira-gp <ruchira.gp99@gmail.com>
2023-08-27 14:39:47 +0530

    added founding page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import AboutLanding from "./About/aboutLanding";
 import '../styles/pages/About/about.scss'
 import Layout from "../layout/Layout";
+import AboutFoundingStory from "./About/aboutFoundingStory";
 export default function About(){
     return(
         <>
         <Layout>
         <AboutLanding></AboutLanding>
-
+        <AboutFoundingStory></AboutFoundingStory>
         </Layout>
         </>
     )
