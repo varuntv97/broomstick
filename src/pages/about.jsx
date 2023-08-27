58851a9a257f7f5958368b662cdd49900be249dd
ruchira-gp <ruchira.gp99@gmail.com>
2023-08-27 15:18:22 +0530

    about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,12 +3,14 @@ import AboutLanding from "./About/aboutLanding";
 import '../styles/pages/About/about.scss'
 import Layout from "../layout/Layout";
 import AboutFoundingStory from "./About/aboutFoundingStory";
+import AboutOurBackers from "./About/aboutOurBackers";
 export default function About(){
     return(
         <>
         <Layout>
         <AboutLanding></AboutLanding>
         <AboutFoundingStory></AboutFoundingStory>
+        <AboutOurBackers></AboutOurBackers>
         </Layout>
         </>
     )
